dacd2fc89f2aa479dfe2ecd3d703fcc24fb24768
ywgfhk <seungchaanfaichaan@gmail.com>
2022-05-20 12:04:46 -0700

    New novels added to config. Some modifications to footnote scraping.  New password capabilities.

--- scraper.py
+++ scraper.py
@@ -9,6 +9,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 
 
@@ -23,14 +24,25 @@ def get_ch_links(driver, TOC_url, ch_link_xpath):
 
 def start_driver():
     # start webdriver
-    driver = webdriver.Chrome(executable_path="C:\\Users\\test\\AppData\\Local\\Programs\\Python\\Python36\\chromedriver.exe")
+    options = webdriver.ChromeOptions()
+    options.add_argument(
+        "user-agent=Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--enable-javascript")
+    options.add_argument('-ignore-certificate-errors')
+    options.add_argument('-ignore -ssl-errors')
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.set_capability("pageLoadStrategy", "eager")
+    driver = webdriver.Chrome(executable_path="C:\\Users\\test\\AppData\\Local\\Programs\\Python\\Python36\\chromedriver.exe", options=options)
     driver.maximize_window()
-    return(driver)
+    return driver
 
 
 def get_body_text(driver, body_xpath):
     print('getting body')
-    # WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//iframe")))
+    #WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//iframe")))
     # element = driver.find_element(By.XPATH, "//iframe")
     # driver.switch_to.frame(element)
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, body_xpath)))
@@ -47,16 +59,26 @@ def make_footnote_counter(start=1):
         return "%d. " % count[0]
     return footnote_counter
 
+
 def html_str_substitutions_for_footnote_formatting(html):
+    #html = re.sub(r'(<li.*?>)(<span>)(.*?)(</span>)(<div>)(.*?)(</div>)(</li>)', r'\1<p>FOOTNOTE_PLACEHOLDER\6</p>\8', html, 0, re.DOTALL) #knoxt
     html = re.sub(r'(<li.*?>)(.*?)(</li>)',r'\1<p>FOOTNOTE_PLACEHOLDER\2</p>\3', html, 0, re.DOTALL)
     html = re.sub(r'(role="doc-endnote"><p>)', r'\1FOOTNOTE_PLACEHOLDER', html)  # temp
     html = re.sub(r'(class="footnote-item"><p>)', r'\1FOOTNOTE_PLACEHOLDER', html)  # temp
+    html = re.sub(r'(<pre class="wp-block-preformatted">)(.*?)(</pre>)',r'\1<p>\2</p>\3', html, 0, re.DOTALL) # inkstainedcharm
+    html = re.sub(r'(<pre class="wp-block-verse">)(.*?)(</pre>)', r'\1<p>\2</p>\3', html, 0,
+                  re.DOTALL)  # novel98tl
     html = re.sub('FOOTNOTE_PLACEHOLDER', make_footnote_counter(), html)
+    return html
+
+def html_str_substitutions_dividers(html):
+    html = html.replace('<hr', "<p>---</p><hr")
     return html
 
 def html_str_substitutions_for_removing_wordpress_links(html):
     xpaths = ["//*[contains(@class, 'related') or contains(@class, 'sd-') or contains(@class, 'post-navigation') or contains(@class, 'pp-multiple-authors')]"]
     return remove_from_html_by_xpath(html, xpaths)
+
 
 def remove_from_html_by_xpath(html, xpaths):
     if not xpaths:
@@ -68,6 +90,7 @@ def remove_from_html_by_xpath(html, xpaths):
             bad.drop_tree()
     return lh.tostring(tree, encoding="unicode")  #if encoding is anything besides "unicode" a byte object is returned
 
+
 def move_footnote_to_bottom(html, footnote_xpath):
     tree = et.ElementTree(lh.fromstring(html))
     root = tree.getroot()
@@ -77,18 +100,27 @@ def move_footnote_to_bottom(html, footnote_xpath):
         tailless_copy = et.tostring(deepcopy(footnote), with_tail=False, encoding="unicode") # use tostring's with_tail parameter to remove tail
         footnote_text = bs(tailless_copy, "lxml").getText() # get footnote text, prepend footnote number insertion, move footnote to end of html
         root.append(lh.fromstring('<p>FOOTNOTE_PLACEHOLDER '+footnote_text+'</p>'))
-    # print(lh.tostring(tree, encoding="unicode"))
     return lh.tostring(tree, encoding="unicode")
+
 
 def html_add_brackets_around_superscript(html):
     html = html.replace('</sup>', "]")
     html = re.sub(r'<sup.*?>', r'[', html)
     return html
 
+
 def decrypt(encoded):
-    #decrypts crysanthemom garden's text scrambling
-    keyMap = dict(zip('jymvfoutlpxiwcbqdgraenkzshCDJGSMXLPABOZRYUHEVKFNQWTI', 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    #decrypts chrysanthemum garden's text scrambling
+    keyMap = dict(zip('jymvfoutlpxiwcbqdgraenkzshCDJGSMXLPABOZRYUHEVKFNQWTI', 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')) #chrysanthemum
     return ''.join(keyMap.get(c, c) for c in encoded)
+
+
+def enter_pwd(driver, pwd, pwd_field_path, submit_btn_xpath):
+    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, pwd_field_path)))
+    pwd_field_element = driver.find_element(By.XPATH, pwd_field_path)
+    # enter pwd and press Enter
+    pwd_field_element.send_keys(pwd)
+    driver.find_element(By.XPATH, submit_btn_xpath).click()
 
 # def url_check(url):
 #     return True
@@ -97,13 +129,19 @@ def close_chapter(file):
     file.write("----------------------------------")
 
 
-def main(first_ch_url, output_filename, body_xpath, chapter_header_xpath ='', next_ch_button_xpath ='', title_must_contain ='', tags =['h1', 'h2', 'h3', 'p', 'cite'], remove_xpath =''):
+def main(first_ch_url, output_filename, body_xpath, chapter_header_xpath ='', next_ch_button_xpath ='', title_must_contain ='', tags =['h1', 'h2', 'h3', 'p', 'cite'], remove_xpath ='', footnote_xpath =''):
     # open output file
     f = open(output_filename, "a", encoding="utf-8")
     # start web crawler
     driver = start_driver()
     driver.get(first_ch_url)
-    driver.implicitly_wait(10)
+
+    #time.sleep(20)
+
+    # # Enter text in pwd box
+    # WebDriverWait(driver, 10).until(
+    #     EC.presence_of_element_located((By.XPATH, "//p/a[contains(@href,'') and contains(text(),'here')]")))
+    # driver.find_element(By.XPATH, "//p/a[contains(@href,'') and contains(text(),'here')]").click()
 
     # press change language to trad chinese
     if first_ch_url.startswith('http://www.jjwxc.net/'):
@@ -209,6 +247,13 @@ def main(first_ch_url, output_filename, body_xpath, chapter_header_xpath ='', ne
                 f.write(bs(p, "lxml").getText())
                 f.write('\n')
         elif first_ch_url.startswith('https://chrysanthemumgarden.com/'):
+
+            #enter pwd
+            pwd_submit_buttons = driver.find_elements(By.XPATH,"//input[@value='Submit password']")
+            if len(pwd_submit_buttons)>0:
+                pwd = "ItsGettingKindOfHot"
+                enter_pwd(driver, pwd, "//input[@name='site-pass']", "//input[@value='Submit password']")
+
             html = get_body_text(driver, body_xpath)
             html = remove_from_html_by_xpath(html, ["//*[contains(@style, 'hidden')]"])
             html = html_str_substitutions_for_footnote_formatting(html)
@@ -254,22 +299,31 @@ def main(first_ch_url, output_filename, body_xpath, chapter_header_xpath ='', ne
 
         elif tags == 'no_tags':
             html = get_body_text(driver, body_xpath)
-            html = move_footnote_to_bottom(html, "//span[contains(@class, 'footnotes')]")
+            if footnote_xpath:
+                html = move_footnote_to_bottom(html, footnote_xpath)
             html = html_str_substitutions_for_removing_wordpress_links(html)
-            html = remove_from_html_by_xpath(html, [remove_xpath])
+            if remove_xpath:
+                html = remove_from_html_by_xpath(html, [remove_xpath])
             html = html_str_substitutions_for_footnote_formatting(html)
             html = html_add_brackets_around_superscript(html)
-            html = html.replace("<br/>", "\n")
-            html = html.replace("<br>", "\n")
+            html = html.replace("<br/>", "<p>\n</p>")
+            html = html.replace("<br>", "<p>\n</p>")
             f.write(bs(html, "lxml").getText())
             f.write('\n')
         else:
+            # pwd_submit_buttons = driver.find_elements(By.XPATH, "//input[@value='Enter']")
+            # if len(pwd_submit_buttons) > 0:
+            #     pwd = "3028499"
+            #     enter_pwd(driver, pwd, "//input[@name='post_password']", "//input[@value='Enter']")
+
             html = get_body_text(driver, body_xpath)
-            #html = move_footnote_to_bottom(html, "//span[contains(@class, 'footnotes')]")
+            if footnote_xpath:
+                html = move_footnote_to_bottom(html, footnote_xpath)
             html = html_str_substitutions_for_removing_wordpress_links(html)
-            #html = remove_from_html_by_xpath(html, [remove_xpath]) # html = remove_from_html_by_xpath(html, '')
-            ##html = move_footnote_to_bottom(html, "//p[contains(@class,'has-small-font-size')]") // estranged
+            if remove_xpath:
+                html = remove_from_html_by_xpath(html, [remove_xpath])
             html = html_str_substitutions_for_footnote_formatting(html)
+            html = html_str_substitutions_dividers(html)
             soup = bs(html, "lxml")
             all_p = soup.find_all(tags)
             for p in all_p:
@@ -285,9 +339,11 @@ def main(first_ch_url, output_filename, body_xpath, chapter_header_xpath ='', ne
         # Is there a next page?
         try:
             next_page_url = driver.find_element(By.XPATH, next_ch_button_xpath).get_attribute('href')
+            #driver.find_element(By.XPATH, next_ch_button_xpath).click()
             # url_check: raise error if url is already visited
             driver.get(next_page_url)
             print("prodeeding to next page: " + next_page_url)
+            #time.sleep(5)
 
         except NoSuchElementException:
             print("Element does not exist")
@@ -310,6 +366,8 @@ def parse_args():
     parser.add_argument("--filter_header", "-f", help="only scrapes page if header contains the following text")
     parser.add_argument("--scrape_tags", "-t", help="Which tags to will be scraped with BeautifulSoup.  Default is ['h1', 'h2', 'h3', 'p', 'cite'].  Use \"no_tags\" to not use BeautifulSoup in cases where text is in the root node.")
     parser.add_argument("--remove", "-rm", help="supply xpath of elements to remove as a list, ex. ['//h1'] or ['//h1','//h2']")
+    parser.add_argument("--footnote", "-fn",
+                        help="supply xpath of elements to move to bottom, ex. ['//h1'] or ['//h1','//h2']")
     #parser.add_argument("--html", help="use flag to save html rather than plaintext", default=False, action='store_true')
     #parser.add_argument(description='scrape text from Wordpress, Wattpadd etc. into one text file', epilog=example_text)
     return parser.parse_args()
@@ -340,6 +398,10 @@ if __name__ == "__main__":
         remove_xpath = args.remove
     else:
         remove_xpath = ''
+    if args.footnote:
+        footnote_xpath = args.footnote
+    else:
+        footnote_xpath = ''
 
-    main(first_ch_url, output_filename, body_xpath, chapter_header_xpath, next_ch_button_xpath, title_must_contain, tags, remove_xpath)
+    main(first_ch_url, output_filename, body_xpath, chapter_header_xpath, next_ch_button_xpath, title_must_contain, tags, remove_xpath, footnote_xpath)
 
